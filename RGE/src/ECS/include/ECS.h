4ceb6e279bc69dab6d0a8d8d0f4031c0666750b2
Darren Tynan <darrentynan@gmail.com>
2024-12-10 17:23:57 +0000

    Moved iPool and Pool to a separate header file. Pool.h

--- RGE/src/ECS/include/ECS.h
+++ RGE/src/ECS/include/ECS.h
@@ -16,6 +16,8 @@
 #include <algorithm>
 #include "../../Logger/Logger.h"
 
+#include "Pool.h"
+
 const unsigned int MAX_COMPONENTS = 32;
 
 /**
@@ -107,38 +109,39 @@ class System {
 };
 
 
-/**
- * @brief IPool is just a vector (contiguous data) of objects of type T
- */
-class IPool {
-    public:
-        virtual ~IPool() = default;
-};
-
-
-/**
- * @brief A Pool class to hold pointers to entities spawned.
- *
- * @tparam T
- */
-template <typename T>
-class Pool: public IPool {
-    private:
-        std::vector<T> data;
-
-    public:
-        explicit Pool(int size = 100) { data.resize(size); }
-        ~Pool() override = default;
-
-        [[nodiscard]] bool IsEmpty() const { return data.empty(); }
-        [[nodiscard]] int GetSize() const { return data.size(); }
-        void Resize(int n) { data.resize(n); }
-        void Clear() { data.clear(); }
-        void Add(T object) { data.push_back(object); }
-        void Set(int index, T object) { data[index] = object; }
-        T& Get(int index) { return static_cast<T&>(data[index]); }
-        T& operator [](unsigned int index) { return data[index]; }
-};
+//TODO moved it to a seperate header file Pool.h
+///**
+// * @brief IPool is just a vector (contiguous data) of objects of type T
+// */
+//class IPool {
+//    public:
+//        virtual ~IPool() = default;
+//};
+//
+//
+///**
+// * @brief A Pool class to hold pointers to entities spawned.
+// *
+// * @tparam T
+// */
+//template <typename T>
+//class Pool: public IPool {
+//    private:
+//        std::vector<T> data;
+//
+//    public:
+//        explicit Pool(int size = 100) { data.resize(size); }
+//        ~Pool() override = default;
+//
+//        [[nodiscard]] bool IsEmpty() const { return data.empty(); }
+//        [[nodiscard]] int GetSize() const { return data.size(); }
+//        void Resize(int n) { data.resize(n); }
+//        void Clear() { data.clear(); }
+//        void Add(T object) { data.push_back(object); }
+//        void Set(int index, T object) { data[index] = object; }
+//        T& Get(int index) { return static_cast<T&>(data[index]); }
+//        T& operator [](unsigned int index) { return data[index]; }
+//};
 
 
 /**
