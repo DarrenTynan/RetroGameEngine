23bbbd4376cb349733ca1e312d1b63ffe6eb1711
Darren Tynan <darrentynan@gmail.com>
2024-12-10 19:28:31 +0000

    Moved Entity class to separate header file.

--- RGE/src/ECS/include/ECS.h
+++ RGE/src/ECS/include/ECS.h
@@ -18,48 +18,7 @@
 
 #include "Pool.h"
 #include "Component.h"
-//#include "Entity.h"
-
-//***********************************************************************
-/**
- * @brief A small class to hold data for entity.
- */
-class Entity
-{
-private:
-    int id;
-public:
-    explicit Entity(int id): id(id) {};
-    Entity(const Entity &entity) = default;
-    [[nodiscard]] int GetId() const;
-    void Kill();
-
-    // Manage entity tags and groups
-    void AddTag(const std::string &tag);
-    [[nodiscard]] bool HasTag(const std::string &tag) const;
-    void Group(const std::string &group);
-    [[nodiscard]] bool BelongsToGroup(const std::string &group) const;
-
-    // Operator overloading for entity objects
-    Entity& operator = (const Entity& other) = default;
-    bool operator == (const Entity& other) const { return id == other.id; }
-    bool operator != (const Entity& other) const { return id != other.id; }
-    bool operator > (const Entity& other) const { return id > other.id; }
-    bool operator < (const Entity& other) const { return id < other.id; }
-
-    // Manage entity components
-    template <typename TComponent, typename ...TArgs> void AddComponent(TArgs&& ...args);
-    template <typename TComponent> void RemoveComponent();
-    template <typename TComponent> [[nodiscard]] bool HasComponent() const;
-    template <typename TComponent> TComponent& GetComponent() const;
-
-    // Hold a pointer to the entity's owner registry
-    class Registry* registry;
-
-};
-
-
-
+#include "Entity.h"
 
 //***********************************************************************
 /**
