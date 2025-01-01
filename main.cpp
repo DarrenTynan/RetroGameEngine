545afc30a7b99e862dd47921007a682ff15c95d5
Darren Tynan <darrentynan@gmail.com>
2025-01-01 17:38:32 +0000

    Added Lua and Sol and test functions in main.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include "Game/include/Game.h"
 
+#include <lua/lua.hpp>
+#include <sol/sol.hpp>
+
+void TestLua()
+{
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    // Call lue code from script file
+    lua.script_file("../Game/scripts/test.lua");
+    // Same as above but an inline script
+    lua.script("print('Hello World from Lua!')");
+
+}
+
 int main()
 {
-    Game::Setup();
-    Game::Run();
-    Game::Destroy();
+//    Game::Setup();
+//    Game::Run();
+//    Game::Destroy();
+
+    TestLua();
 
     std::cout << "main.cpp exit" << std::endl;
     return 0;
