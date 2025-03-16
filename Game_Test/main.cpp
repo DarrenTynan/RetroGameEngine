9fa40ca4f3fe8eed7c1f94328cd4e7e41ae0d0f9
Darren Tynan <darrentynan@gmail.com>
2025-03-16 17:13:29 +0000

    Test of library call

---
+++ Game_Test/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Darren Tynan on 16/02/2025.
+//
+
+#include <iostream>
+#include "include/RGE.h"
+
+int main()
+{
+    RGE::InitialSetup();
+
+    std::cout << "main.cpp exit" << std::endl;
+    return 0;
+}
+
